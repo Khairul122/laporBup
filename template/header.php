5b44a7c6bc58f07e8a933b9f17a3f6d3bfe2ffd2
Khairul122 <khairulhuda242@gmail.com>
2025-10-29 04:16:07 +0700

    docs(template): update page title from PPID to Lapor Bupati

--- template/header.php
+++ template/header.php
@@ -6,7 +6,7 @@
   <meta
     name="viewport"
     content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <title>PPID</title>
+  <title>Lapor Bupati</title>
   <link rel="stylesheet" href="assets/vendors/feather/feather.css" />
   <link
     rel="stylesheet"
